c475269f878f96ff5be4ad1f22e01a603dd9a763
João Duarte <115634367+joaodrduarte@users.noreply.github.com>
2022-11-03 12:37:38 +0000

    RegLog by JavaGuides Creation

---
+++ Java Guides/RegLog/src/main/java/tutorial/javaguidesyt/RegLog/RegLogApplication.java
@@ -0,0 +1,13 @@
+package tutorial.javaguidesyt.RegLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegLogApplication.class, args);
+	}
+
+}
