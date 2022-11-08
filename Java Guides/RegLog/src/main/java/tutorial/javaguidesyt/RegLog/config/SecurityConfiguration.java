75244fe36de8213a20527fbbcd497c5e1bfab303
João Duarte <115634367+joaodrduarte@users.noreply.github.com>
2022-11-08 14:52:54 +0000

    Update SecurityConfiguration.java

--- Java Guides/RegLog/src/main/java/tutorial/javaguidesyt/RegLog/config/SecurityConfiguration.java
+++ Java Guides/RegLog/src/main/java/tutorial/javaguidesyt/RegLog/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -52,4 +53,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login?logout")
                 .permitAll();
     }
+
 }
